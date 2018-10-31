c58e90b8c2f20ad876c965ed62ec7dc394c39968
jwiddison <jordan.widdison@gmail.com>
2018-10-30 23:46:08 -0600

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+import Hooks from "./hooks";
+import NoHooks from "./hooks";
+
+export { Hooks, NoHooks };
---
+++ src/components/noHooks.js
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export default function NoHooks(props) {
+  const name = "asdf";
+
+  return <section>Test</section>;
+}
