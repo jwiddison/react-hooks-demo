b73d2cdb418f21c17548af1f362cf2d43c5482f9
Trevor Fenn <sgtpepper43@gmail.com>
2018-11-05 15:44:49 -0700

    Add apollo demo

--- src/components/hooks.js
+++ src/components/hooks.js
@@ -87,3 +87,106 @@ export default function Hooks(props) {
 //     </section>
 //   );
 // }
+//
+//
+// --------------------
+// Using apollo:
+// --------------------
+//
+// import { useQuery, useMutation } from '../lib/apollo';
+//
+// function useFormInput(initialValue) {
+//   const [value, setValue] = useState(initialValue);
+//
+//   function handleChange(e) {
+//     setValue(e.target.value);
+//   }
+//
+//   return {
+//     value,
+//     onChange: handleChange
+//   };
+// }
+//
+// const USER_QUERY = `
+//   query user($id: ID) {
+//     user(id: $id) {
+//       firstName
+//       lastName
+//     }
+//   }
+// `;
+//
+// const USER_MUTATION = `
+//   mutation userUpdate(
+//     $id: ID
+//     $firstName: String
+//     $lastName: String
+//   ) {
+//     userUpdate(
+//       id: $id
+//       firstName: $firstName
+//       lastName: $lastName
+//     ) {
+//       id
+//       firstName
+//       lastName
+//     }
+//   }
+// `;
+//
+// export default function Hooks() {
+//   const id = useFormInput('1');
+//   const { data, loading } = useQuery({ query: USER_QUERY, variables: { id: id.value } });
+//   const [mutate, { loading: saving }] = useMutation({
+//     mutation: USER_MUTATION,
+//     variables: { id: id.value }
+//   });
+//
+//   return (
+//     <section>
+//       <h1>Demo With Hooks</h1>
+//       <label><span>ID</span> <input {...id} type="number" step="1" min="1" /></label>
+//       <br />
+//       {loading ? <LoadingForm /> : (
+//         <UserForm
+//           key={id.value}
+//           user={(data && data.user) || {}}
+//           saveUser={mutate}
+//           saving={saving}
+//         />
+//       )}
+//     </section>
+//   );
+// }
+//
+// function LoadingForm() {
+//   return (
+//     <React.Fragment>
+//       <label>First Name <input className="loading" /></label>
+//       <br />
+//       <label>Last Name <input className="loading" /></label>
+//     </React.Fragment>
+//   );
+// }
+//
+// function UserForm({ user, saveUser, saving }) {
+//   const firstName = useFormInput(user.firstName);
+//   const lastName = useFormInput(user.lastName);
+//
+//   function handleSubmit(e) {
+//     e.preventDefault();
+//     saveUser({ firstName: firstName.value, lastName: lastName.value });
+//   }
+//
+//   const inputClass = saving ? 'loading' : 'fade-in';
+//
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <label>First Name <input {...firstName} className={inputClass} /></label>
+//       <br />
+//       <label>Last Name <input {...lastName} className={inputClass} /></label>
+//       <div className="button-wrapper"><button disabled={saving}>Save</button></div>
+//     </form>
+//   );
+// }
